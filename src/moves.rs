f37ac05d78813dfb40839c93f468c0a405ac023a
gan74 <gregoire.angerand@gmail.com>
2020-02-13 23:22:37 +0100

    Fixed pawn being able to capture when moving straight

--- src/moves.rs
+++ src/moves.rs
@@ -41,7 +41,7 @@ pub fn possible_moves_for_piece(board: &Board, colored: ColoredPiece, pos: Pos) 
             }
 
             let dst_row = cmp::min(cmp::max(row as isize + dir, 0), 7) as usize;
-            let mut pawn = path(enemies, allies, pos, col, dst_row);
+            let mut pawn = path(enemies, allies, pos, col, dst_row, false);
 
             // captures
             {
@@ -60,25 +60,25 @@ pub fn possible_moves_for_piece(board: &Board, colored: ColoredPiece, pos: Pos) 
         }
 
         Piece::Rook => {
-            let n = path(enemies, allies, pos, col, 7);
-            let s = path(enemies, allies, pos, col, 0);
-            let e = path(enemies, allies, pos, 7, row);
-            let w = path(enemies, allies, pos, 0, row);
+            let n = path(enemies, allies, pos, col, 7, true);
+            let s = path(enemies, allies, pos, col, 0, true);
+            let e = path(enemies, allies, pos, 7, row, true);
+            let w = path(enemies, allies, pos, 0, row, true);
             n.with_board(s).with_board(w).with_board(e)
         }
         
         Piece::Bishop => {
             let diff = cmp::min(col, row);
-            let a = path(enemies, allies, pos, col - diff, row - diff);
+            let a = path(enemies, allies, pos, col - diff, row - diff, true);
             
             let diff = cmp::min(7 - col, row);
-            let b = path(enemies, allies, pos, col + diff, row - diff);
+            let b = path(enemies, allies, pos, col + diff, row - diff, true);
             
             let diff = cmp::min(col, 7 - row);
-            let c = path(enemies, allies, pos, col - diff, row + diff);
+            let c = path(enemies, allies, pos, col - diff, row + diff, true);
             
             let diff = cmp::min(7 - col, 7 - row);
-            let d = path(enemies, allies, pos, col + diff, row + diff);
+            let d = path(enemies, allies, pos, col + diff, row + diff, true);
             
             a.with_board(b).with_board(c).with_board(d)
         }
@@ -137,7 +137,7 @@ fn is_valid(w: isize) -> bool {
     w >= 0 && w < 8
 }
 
-fn path(enemies: BitBoard, allies: BitBoard, start: Pos, end_col: usize, end_row: usize) -> BitBoard {
+fn path(enemies: BitBoard, allies: BitBoard, start: Pos, end_col: usize, end_row: usize, can_capture: bool) -> BitBoard {
     let mut d_col = (end_col as isize) - (start.col() as isize);
     let mut d_row = (end_row as isize) - (start.row() as isize);
     let s_col = d_col.signum();
@@ -185,10 +185,10 @@ fn path(enemies: BitBoard, allies: BitBoard, start: Pos, end_col: usize, end_row
         debug_assert!(is_pos_valid(col, row));
         
         let p = Pos::new(col as usize, row as usize);
-        if allies.piece_at(p) {
+        if allies.piece_at(p) || (!can_capture && enemies.piece_at(p)) {
             break;
         }
-        
+
         board.add(p);
         if enemies.piece_at(p) {
             break;
